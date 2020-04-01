313655c8a26a3039ceb36d03fed5ae54fe97ffbc
Cornelius Okeke <okekecornelius@gmail.com>
2020-04-01 20:21:02 +0100

    update on environment variable

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   _STATE: "https://locationsng-api.herokuapp.com/api/v1/",
+  _STATE_2: "https://api.facts.ng/v1/",
   _SERVER: "http://localhost:3900/api/v1/"
 };
