04237fcedcbc5e072def6a66aa365446d04ad86d
Cornelius Okeke <okekecornelius@gmail.com>
2020-04-06 15:19:11 +0100

    Update... implemented a questionaire form

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -4,12 +4,12 @@ import { NgForm } from '@angular/forms';
 import { Subscription } from 'rxjs';
 import _ from "underscore";
 
-
 import { IState } from '../../interfaces/state';
+
 import { StateService } from '../state/state.service';
-import { CaseService } from '../case/case.service';
 import { AuthService } from '../auth/auth.service';
-import { NavigationService } from 'src/app/shared/service/navigation.service';
+import { NavigationService } from '../../shared/service/navigation.service';
+import { NotificationService } from '../../shared/service/notification.service';
 
 @Component({
   selector: 'app-home',
@@ -29,31 +29,82 @@ export class HomeComponent implements OnInit {
     public authService: AuthService,
     private stateService: StateService,
     private navigationService: NavigationService,
-    private caseService: CaseService
+    private notificationService: NotificationService
   ) { }
 
 
 
-  onSubmitQuestionaire(form: NgForm) {
-    const pre = "+234"
-    if (form.invalid) {
-      return;
-    }
 
-    console.log(form.value.isCough)
-    // this.caseService.addCase
-    //   (
-    //     form.value.inputFirstname,
-    //     form.value.inputSurname,
-    //     pre + form.value.inputPhoneNumber,
-    //     form.value.inputStateName,
-    //     form.value.inputLGA,
-    //     form.value.inputDOB,
-    //     form.value.inputAddress,
-    //     form.value.inputSymptoms
-    //   );
+  onSubmitQuestionaire(form: NgForm) {
+
+    this.questionareCalculation
+      (
+        form.value.isFever,
+        form.value.isCough,
+        form.value.isBlockedNose,
+        form.value.isTired,
+        form.value.isDifficultBreathing,
+        form.value.isGotoCountry,
+        form.value.isDirectContact
+      );
 
   }
+
+
+  questionareCalculation(
+    fever: boolean,
+    cough: boolean,
+    blockedNose: boolean,
+    feelingTired: boolean,
+    difficultBreathing: boolean,
+    goneCountry: boolean,
+    directContact: boolean
+  ) {
+    let totalPoint = 20;
+    let isFever = 0;
+    let isCough = 0;
+    let isBlockedNose = 0;
+    let isTired = 0;
+    let isDifficultBreathing = 0;
+    let isGotoCountry = 0;
+    let isDirectContact = 0;
+    let sum = 0;
+
+
+    if (fever) {
+      isFever = 2
+    }
+    if (cough) {
+      isCough = 2
+    }
+    if (blockedNose) {
+      isBlockedNose = 2
+    }
+    if (feelingTired) {
+      isTired = 2
+    }
+    if (difficultBreathing) {
+      isDifficultBreathing = 4
+    }
+    if (goneCountry) {
+      isGotoCountry = 4
+    }
+    if (directContact) {
+      isDirectContact = 4
+    }
+    sum = isFever + isCough + isBlockedNose + isTired + isDifficultBreathing + isGotoCountry + isDirectContact;
+
+    let result = sum / totalPoint * 100
+
+    this.notificationService.smallSuccess(`${result}%`);
+    if (result >= 50) {
+      this.notificationService.infected('You might have contacted COVID-19');
+      this.navigationService.goToOn_boarding();
+    } else {
+      this.notificationService.notInfected('Goodnews!!!');
+    }
+  }
+
 
   initContents() {
     this.isAuthenticated = this.authService.getIsAuthenticated()
--- src/app/shared/service/notification.service.ts
+++ src/app/shared/service/notification.service.ts
@@ -38,80 +38,6 @@ export class NotificationService {
     })
   }
 
-  smallSuccessRight(message) {
-    const toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 5000,
-      grow: 'column'
-    })
-
-    toast.fire({
-      icon: 'success',
-      title: message
-    })
-  }
-
-  smallWarning(message) {
-    const toast = Swal.mixin({
-      toast: true,
-      position: 'top',
-      showConfirmButton: false,
-      timer: 5000,
-      grow: 'column'
-    })
-
-    toast.fire({
-      icon: 'warning',
-      title: message
-    })
-  }
-
-  smallError(message) {
-    const toast = Swal.mixin({
-      toast: true,
-      position: 'top',
-      showConfirmButton: false,
-      timer: 5000,
-      grow: 'column'
-    })
-
-    toast.fire({
-      icon: 'error',
-      title: message
-    })
-  }
-
-
-
-  notAllowed(message) {
-    const toast = Swal.mixin({
-      toast: true,
-      position: 'top-end',
-      showConfirmButton: false,
-      timer: 5000,
-      grow: 'column'
-    })
-
-    toast.fire({
-      icon: 'error',
-      title: message
-    })
-
-    // Swal.fire({
-    //   position: 'top-end',
-    //   icon: 'success',
-    //   title: message,
-    //   showConfirmButton: false,
-    //   timer: 1500
-    // })
-
-    // this.config.panelClass = ['notification', 'success'];
-    // this.snackBar.open(message, '', this.config);
-  }
-
-
   error(message) {
     Swal.fire({
       icon: 'error',
@@ -123,61 +49,23 @@ export class NotificationService {
 
   }
 
-
-  serverError(message) {
+  notInfected(message) {
     Swal.fire({
-      icon: 'error',
-      title: 'Oops...',
-      text: message,
-    })
-  }
-
-  confirmDialog() {
-    const swalWithBootstrapButtons = Swal.mixin({
-      customClass: {
-        confirmButton: 'btn btn-success',
-        cancelButton: 'btn btn-danger'
-      },
-      buttonsStyling: false
-    })
-
-    swalWithBootstrapButtons.fire({
-      title: 'Are you sure?',
-      text: "You won't be able to revert this!",
-      icon: 'warning',
-      showCancelButton: true,
-      confirmButtonText: 'Yes, delete it!',
-      cancelButtonText: 'No, cancel!',
-      reverseButtons: true
-    }).then((result) => {
-      if (result.value) {
-        swalWithBootstrapButtons.fire(
-          'Deleted!',
-          'Your file has been deleted.',
-          'success'
-        )
-      } else if (
-        /* Read more about handling dismissals below */
-        result.dismiss === Swal.DismissReason.cancel
-      ) {
-        swalWithBootstrapButtons.fire(
-          'Cancelled',
-          'Your imaginary file is safe :)',
-          'error'
-        )
-      }
-    })
-  }
-
-
-  wrongUser(message) {
-    Swal.fire({
-      icon: 'error',
+      icon: 'info',
       title: message,
-      text: "Kindly login through the mobile applications. Thanks",
+      text: "Your symptoms aren't enough evidence, you come back when the symptoms seem to be increasing",
       timer: 7000
 
     })
+  }
 
+  infected(message) {
+    Swal.fire({
+      icon: 'info',
+      title: message,
+      text: "Redirecting you to register page",
+      timer: 7000
+
+    })
   }
 }
